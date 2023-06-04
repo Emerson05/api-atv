a27c2bf9b99d3c04c66cee9f9c25db37612cfa80
Emerson05 <emerosnjuniormt@gmail.com>
2023-06-03 22:07:56 -0300

    atividade consumo api react

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, Alert, Image, Text } from 'react-native';
+import { styles } from './style';
+
+const API_KEY = '8fea875db776ddfc97976e0ad2458d6e';
+
+const Formulario = () => {
+  const [cidade, setCidade] = useState('');
+  const [clima, setClima] = useState(null);
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${API_KEY}&units=metric`);
+
+      if (response.ok) {
+        const data = await response.json();
+        setClima(data);
+      } else {
+        Alert.alert('Erro', 'Ocorreu um erro ao buscar o clima.');
+      }
+    } catch (error) {
+      Alert.alert('Erro', 'Ocorreu um erro ao buscar o clima.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={require('./img/logo.png')} style={styles.logo} />
+      <TextInput 
+        value={cidade}
+        onChangeText={setCidade}
+        style={styles.input}
+        placeholder="Digite o nome da cidade"
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={{ backgroundColor: '#FFFF', padding: 10, color: '#4F709C', borderRadius: 20 }}>
+          Buscar clima 
+        </Text>
+      </TouchableOpacity>
+       
+      {clima && (
+        <View style={{ color: '#FFFF', marginTop: 10 }}>
+          <Text>Local: {clima.name}</Text>
+          <Text>Temperatura: {clima.main.temp}°C</Text>
+          <Text>Condição: {clima.weather[0].description}</Text>
+        </View>
+      )}
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', padding: 16, backgroundColor: '#4F709C', alignContent:'center', justifyContent:'center' }}>
+      <Formulario />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ style.js
@@ -0,0 +1,30 @@
+// styles.js
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+    backgroundColor: '#4F709C',
+    alignItems: 'center'
+  },
+  logo: {
+    marginBottom: 20,
+    width: 200,
+    height: 100,
+   
+    alignItems: 'center'
+  },
+  input: {
+    height: 50,
+    width: 300,
+    color: 'black',
+    backgroundColor: '#FFFF',
+    borderColor: 'gray',
+    borderWidth: 3,
+    marginBottom: 10,
+    
+  },
+ 
+});
